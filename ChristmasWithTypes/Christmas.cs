2e2a6aecac2511632c8695621f602bf391ac20d1
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-04-21 13:31:14 -0400

    I think I finally figured it out haha fingers crossed

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -4,8 +4,8 @@ namespace ChristmasWithTypes
     public class Christmas
     {
         public string[] Presents;
-        public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public string Santa = "Kris Kringle";
+        public int? TreeHeight = null; //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
         public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
